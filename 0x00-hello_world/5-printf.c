af519706385e9030335e6f9fb5bd078ad8c7c9e8
Isaac Katusabe <isaackatusabe@gmail.com>
2022-06-30 15:12:27 -0700

    printing to stdout using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdout>
+/**
+ * main - prints the size of various types
+ * Return:0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
