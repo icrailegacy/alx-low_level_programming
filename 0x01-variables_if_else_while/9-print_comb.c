4c40ff419ecd4ea186f0a5366f02e8d627d0f949
Isaac Katusabe <isaackatusabe@gmail.com>
2022-07-03 02:05:56 -0700

    Write a program that
     prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,7 +5,7 @@
  * main - Print all possible combinations of single-digit numbers.
  *Description - Numbers must be separated by commas and a space.
  * Numbers must be in ascending order
- * You can only use the putchar function 
+ * You can only use the putchar function
  * You can only use putchar a maximum of four times.
  * You are not allowed to use any variable of type `char`.
  * Return: Always 0
