f920b672ec415e5461202b2245b06b64627d71f2
Isaac Katusabe <isaackatusabe@gmail.com>
2022-07-04 04:59:00 -0700

    a

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,14 @@
 
 int main(void)
 {
-	int ch;
-	ch != 101;
-	ch != 113;
-	for (ch = 97; ch <= 122; ch++)
-		{
+	int ch = 97;
+
+	while (ch <= 122, ch != 101, ch != 113)
+	{
 		putchar(ch);
-		}
 	}
 	putchar(10);
+	ch++;
 
 	return (0);
 }
