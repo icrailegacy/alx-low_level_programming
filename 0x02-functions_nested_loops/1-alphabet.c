079c9e2b542b153c5e34d603af5eafe281b837bd
Isaac Katusabe <isaackatusabe@gmail.com>
2022-07-05 15:32:40 -0700

    print the alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+*main - print the alphabet in lower case followed by a new line
+*return: always 0
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+		_putchar('c');
+	}
+	_putchar('\n');
+	return (0);
+
+}
