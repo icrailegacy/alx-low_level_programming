50fcde8859470835a460ab42b8a3e4d0c05ecbab
Isaac Katusabe <isaackatusabe@gmail.com>
2022-07-06 10:06:55 -0700

    add 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers and returns the result
+ * @a: An integer a
+ * @b: An integer b
+ * Description: use _putchar function
+ * Return: Result to add a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
