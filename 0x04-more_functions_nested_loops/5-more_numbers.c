7bbecf4206fe98a0c51cadc4e116c863ab64958b
Isaac Katusabe <isaackatusabe@gmail.com>
2022-07-07 23:02:02 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,21 @@
 
 void more_numbers(void)
 {
-int n, i;
+	int i;
+	int x;
 
-for (n = 0; n < 10; n++)
-{
-for (i = 0; i <= 14; i++)
-{
-if (i >= 10)
-{
-_putchar((i / 10) + '0');
-}
-_putchar((i % 10) + '0');
-}
-_putchar('\n');
-
-return 0;
+	for (x = 0; x < 10; x++)
+	{
+		i = 0;
+		while (i <= 14)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+				_putchar(i % 10 + '0');
+			i++;
+		}
+	_putchar('\n');
+	}
 }
