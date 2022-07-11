5e21e326a228d2ee89157d01609a151ea48442ff
Isaac Katusabe <isaackatusabe@gmail.com>
2022-07-11 20:35:09 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,7 +5,7 @@
 *main - a function that takes a pointer to an int as parameter and updates the value it points to to 98
 *Return: Always 0
 */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
  int n; 
  int p = &n;
