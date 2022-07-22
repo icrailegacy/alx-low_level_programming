f8989114e81fe36ead1640b67c564a52aec802cc
Isaac Katusabe <isaackatusabe@gmail.com>
2022-07-22 23:03:54 +0300

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+/**
+*_isdigit - Checks for digits (0 through 9)
+*@c: int type parameter
+*Return: 1 if digit, else 0
+*/
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	return (1);
+
+	return (0);
+}
