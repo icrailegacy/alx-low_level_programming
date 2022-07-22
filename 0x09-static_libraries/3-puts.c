0f494b755d9ac85036a2adf3769b7cc9fc4f65e2
Isaac Katusabe <isaackatusabe@gmail.com>
2022-07-22 23:06:56 +0300

    Create 3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
