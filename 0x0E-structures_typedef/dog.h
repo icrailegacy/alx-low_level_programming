49e3bc6d348d1fff8ce438c955afd0ea0c0777a0
Isaac Katusabe <isaackatusabe@gmail.com>
2022-08-01 19:37:36 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
 /**
 * struct dog - structure fo document dog's name, age and owner
 * @name: the dog's name
@@ -5,8 +8,21 @@
 * @owner: the dog'd owner's name
 */
 
-struct dog{
+struct dog
+{
   char *name;
   float age;
   char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
