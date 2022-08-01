c0ead0d48c4af9e729329ee6da754ef934822863
Isaac Katusabe <isaackatusabe@gmail.com>
2022-08-01 19:25:38 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+* struct dog - structure fo document dog's name, age and owner
+* @name: the dog's name
+* @age: the dog's age
+* @owner: the dog'd owner's name
+*/
+
+struct dog{
+  char *name;
+  float age;
+  char *owner;
+};
