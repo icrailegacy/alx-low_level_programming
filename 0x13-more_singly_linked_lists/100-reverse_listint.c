60e7a84a50ebabdb51e1999b011ea60f44e926cb
Isaac Katusabe <isaackatusabe@gmail.com>
2022-08-16 22:52:17 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -3,7 +3,7 @@
 /**
  * reverse_listint - reverses a listint_t linked list.
  * Prototype: listint_t *reverse_listint(listint_t **head);
-* Returns: a pointer to the first node of the reversed list
+ * Returns: a pointer to the first node of the reversed list
  * You are not allowed to use more than 1 loop.
  * You are not allowed to use malloc, free or arrays
  * You can only declare a maximum of two variables in your function
